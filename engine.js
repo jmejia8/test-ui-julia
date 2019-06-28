bd38d4c5970ebaa03f59471ed46796dd7f76b5ff
Jesús Mejía <jesusmejded@gmail.com>
2019-06-28 00:43:03 -0500

    base files

---
+++ engine.js
@@ -0,0 +1,7 @@
+function main(){
+    var box = document.getElementById("box2");
+
+    box.style.background = box.style.background = "red"
+
+
+}
--- main.jl
+++ main.jl
@@ -4,27 +4,41 @@ function myfun(w, arg)
 
     println("perro")
     div_id = "box"
-        
-    js_(w, Blink.JSString("""document.getElementById('box').style.background = 'blue'"""))
-    # println(typeof(color))
-    # if color != "red"
-    #     js(w, Blink.JSString("""document.getElementById("$div_id").style= 'background:green;'"""), async=true)
-    # else            
-    #     js(w, Blink.JSString("""document.getElementById("$div_id").style= 'background:red;'"""), async=true)
-    # end
+
+    @js_ w begin
+        @var box = document.getElementById("box")
+        box.style.background = box.style.background == "blue" ? "green" : "blue"
+
+        document.main()
+    end
+
 end
 
 function main()
     w = Window(async=true);
+    load!(w, "materialize/js/materialize.js")
+    load!(w, "materialize/css/materialize.min.css")
+
+
     # Set up julia to handle the "press" message:
     handle(w, "press") do args
         @show args
         myfun(w, 12)
     end
-    # Invoke the "press" message from javascript whenever this button is pressed:
-    body!(w, """<center>Automated Parameter Tuning</center>
-                <button onclick='Blink.msg("press", "HELLO 2")'>Pon color</button>
-                <div id="box" style="background:red; with:50px;height:50px;"></div>""",async=true);
+
+    title(w, "BCAP")
+    progress(w)
+
+
+
+    body!(w, read("index.html", String))
+    load!(w, "style.css")
+    load!(w, "engine.js")
+
+
+    # body!(w, """<center>Automated Parameter Tuning</center>
+    #             <button onclick='Blink.msg("press", "HELLO 2")'>Pon color</button>
+    #             <div id="box" style="background:red; with:50px;height:50px;"></div>""", async=false);
 
 
 end
