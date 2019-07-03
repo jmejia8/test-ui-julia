1d0e49ae651443a2cceb4c068796d4d90a02b0cf
Jesus Mejia <jesusmejded@gmail.com>
2019-07-03 16:22:07 -0500

    project list is working

--- projects.jl
+++ projects.jl
@@ -1,6 +1,43 @@
 using Blink
 import JSON
 
+include("createProject.jl")
+include("runProject.jl")
+
+function jsonToHTML(json_name)
+    json = JSON.parsefile(json_name)
+
+    str = """
+          <tr>
+            <td>$(json["project-name"])</td>
+            <td>$(json["target-algorithm-name"])</td>
+            <td>$(length(json["parameters"]["parameters"]))</td>
+            <td>
+                <a class="waves-effect waves-light btn" onclick="Blink.msg('runProject', '$(json_name)')">Run</a>
+                <a class="waves-effect waves-light btn" onclick="Blink.msg('editProject', '$(json_name)')">Edit</a>
+            </td>
+          </tr>
+    """; 
+    
+end
+
+function init(w)
+    myhomepath = joinpath( homedir(), ".bcap")
+    projectpath = joinpath(myhomepath, "projects")
+
+    str = ""
+
+    for fname in readdir(projectpath)
+        str = string(str, jsonToHTML(joinpath(projectpath, fname)))
+    end
+
+    @js_ w begin
+        @var box = document.getElementById("my-projects")
+        
+        box.innerHTML = $str
+    end
+end
+
 function main_projects()
     w = Window(async=false);
     title(w, "My Projects")
@@ -13,10 +50,27 @@ function main_projects()
 
 
 
-    # handle(w, "init") do args
-    #     println("Initializing...")
-    #     init(w)
-    # end
+    handle(w, "init") do args
+        println("Initializing...")
+        init(w)
+    end
+
+    handle(w, "newProject") do args
+        main_createProject()
+
+        @js w begin
+            window.close()
+        end
+    end
+
+    handle(w, "runProject") do json_name
+        println(json_name)
+
+        main_runProject(json_name)
+        @js w begin
+            window.close()
+        end
+    end
 
     load!(w, "engine.js")
     load!(w, "style.css")
